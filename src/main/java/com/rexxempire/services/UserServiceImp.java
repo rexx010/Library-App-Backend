152d38c05b3e04f40386776f5ca59b84933b4b5d
rexx010 <grexx200@gmail.com>
2025-09-12 02:20:32 +0100

    created the userservice which implements the user service interface

--- src/main/java/com/rexxempire/services/UserServiceImp.java
+++ src/main/java/com/rexxempire/services/UserServiceImp.java
@@ -1,4 +1,45 @@
 package com.rexxempire.services;
 
-public class UserServiceImp {
+import com.rexxempire.data.models.User;
+import com.rexxempire.data.repositories.UserRepository;
+import com.rexxempire.dtos.requests.UserRequest;
+import com.rexxempire.dtos.responses.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserResponse registerUser(UserRequest userRequest) {
+        if (userRepository.findByEmail(userRequest.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+        User user = new User();
+        user.setUsername(userRequest.getUsername());
+        user.setEmail(userRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        user.setRole(userRequest.getRole());
+        User savedUser = userRepository.save(user);
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setUsername(savedUser.getUsername());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setRole(savedUser.getRole());
+        return  userResponse;
+    }
+
+    @Override
+    public User validateUser(String email, String password) {
+        return userRepository.findByEmail(email)
+                .filter(user -> passwordEncoder.matches(password, user.getPassword()))
+                .orElse(null);
+
+    }
+
 }
