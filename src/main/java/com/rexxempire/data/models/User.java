ba992351c61f5f243a81135f7a331d5f8a371513
rexx010 <grexx200@gmail.com>
2025-09-12 02:13:34 +0100

    updated my user class

--- src/main/java/com/rexxempire/data/models/User.java
+++ src/main/java/com/rexxempire/data/models/User.java
@@ -1,4 +1,16 @@
 package com.rexxempire.data.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Data
 public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
 }
