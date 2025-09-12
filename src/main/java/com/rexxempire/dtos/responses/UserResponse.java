7e9e753a03a82edd66ed3f0fee2d651bb33b7c3f
rexx010 <grexx200@gmail.com>
2025-09-12 02:17:47 +0100

    my user response return selected information i want the user to get after their registration

--- src/main/java/com/rexxempire/dtos/responses/UserResponse.java
+++ src/main/java/com/rexxempire/dtos/responses/UserResponse.java
@@ -1,4 +1,12 @@
 package com.rexxempire.dtos.responses;
 
+import lombok.Data;
+
+@Data
 public class UserResponse {
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
 }
