cefa3789c3963eed12be6cedf08d83cd3c8d855b
rexx010 <grexx200@gmail.com>
2025-09-12 02:18:23 +0100

    created a user service interface

--- src/main/java/com/rexxempire/services/UserService.java
+++ src/main/java/com/rexxempire/services/UserService.java
@@ -1,4 +1,10 @@
 package com.rexxempire.services;
 
+import com.rexxempire.data.models.User;
+import com.rexxempire.dtos.requests.UserRequest;
+import com.rexxempire.dtos.responses.UserResponse;
+
 public interface UserService {
+    UserResponse registerUser(UserRequest userRequest);
+    User validateUser(String email, String password);
 }
