34c4b27a873dd550f59b755d2b34cfcc7f6f20d2
rexx010 <grexx200@gmail.com>
2025-09-13 07:05:11 +0100

    added BookRepository interface for MongoDB operations on Book model

---
+++ src/main/java/com/rexxempire/data/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.rexxempire.data.repositories;
+
+import com.rexxempire.data.models.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends MongoRepository<Book, String> {
+    Optional<Book> findByIsbn(String isbn);
+}
