c980dc0792ad7eefb029b131886d1375fcf428c8
rexx010 <grexx200@gmail.com>
2025-09-13 07:51:39 +0100

    updated BookLoan model to use @DBRef for user and book references

--- src/main/java/com/rexxempire/data/models/BookLoan.java
+++ src/main/java/com/rexxempire/data/models/BookLoan.java
@@ -2,6 +2,7 @@ package com.rexxempire.data.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -11,8 +12,10 @@ import java.time.LocalDateTime;
 public class BookLoan {
     @Id
     private String id;
-    private String userId;
-    private String bookId;
+    @DBRef
+    private User user;
+    @DBRef
+    private Book book;
     private LocalDateTime loanDate;
     private LocalDateTime returnDate;
     private Status status;
