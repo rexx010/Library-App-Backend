43299aef3c9dcfdf352213360879969a4c9a9a4f
rexx010 <grexx200@gmail.com>
2025-09-14 18:24:46 +0100

    added findFirstByUserIdAndBookIdAndStatus method in BorrowBookRepository

--- src/main/java/com/rexxempire/data/repositories/BorrowBookRepository.java
+++ src/main/java/com/rexxempire/data/repositories/BorrowBookRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface BorrowBookRepository extends MongoRepository <BorrowBook, String>{
     Optional<BorrowBook> findByUserId(String userId);
     Optional<BorrowBook> findByUserIdAndBookIdAndStatus(String userId, String bookId, Status status);
+    Optional<BorrowBook> findFirstByUserIdAndBookIdAndStatus(String userId, String bookId, Status status);
 }
