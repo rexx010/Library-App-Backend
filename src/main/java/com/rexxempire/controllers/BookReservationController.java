9825bfc0752656993f00db345ea383c9f8b74e8d
rexx010 <grexx200@gmail.com>
2025-09-14 18:22:54 +0100

    added BookReservationController with basic endpoint mapping

---
+++ src/main/java/com/rexxempire/controllers/BookReservationController.java
@@ -0,0 +1,10 @@
+package com.rexxempire.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/book-reservations")
+public class BookReservationController {
+
+}
