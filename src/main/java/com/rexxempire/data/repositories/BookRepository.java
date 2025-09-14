f0f0ba895bf21f4dd83d155068434f571a26807a
rexx010 <grexx200@gmail.com>
2025-09-14 05:10:54 +0100

    updated BookRepository to use findById method instead of findByIsbn for consistency with other repositories

--- src/main/java/com/rexxempire/data/repositories/BookRepository.java
+++ src/main/java/com/rexxempire/data/repositories/BookRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface BookRepository extends MongoRepository<Book, String> {
-    Optional<Book> findByIsbn(String isbn);
+    Optional<Book> findById(String id);
 }
