c1b5e8f5e1e5e2d410c14ba7bf0c2eca070aa9e1
rexx010 <grexx200@gmail.com>
2025-09-14 18:23:15 +0100

    added findByUserIdAndBookIdAndStatus method in BookReservationRepository

--- src/main/java/com/rexxempire/data/repositories/BookReservationRepository.java
+++ src/main/java/com/rexxempire/data/repositories/BookReservationRepository.java
@@ -1,7 +1,11 @@
 package com.rexxempire.data.repositories;
 
 import com.rexxempire.data.models.BookReservation;
+import com.rexxempire.data.models.ReservationStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface BookReservationRepository extends MongoRepository <BookReservation, String>{
+    Optional<BookReservation> findByUserIdAndBookIdAndStatus(String userId, String bookId, ReservationStatus reservationStatus);
 }
