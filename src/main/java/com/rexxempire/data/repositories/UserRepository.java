ed0fc26b1bb214ecb8c2077f2c61322486db3639
rexx010 <grexx200@gmail.com>
2025-09-14 05:13:51 +0100

    updated UserRepository to include findByUsername method and restored findByEmail query

--- src/main/java/com/rexxempire/data/repositories/UserRepository.java
+++ src/main/java/com/rexxempire/data/repositories/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String username);
 }
