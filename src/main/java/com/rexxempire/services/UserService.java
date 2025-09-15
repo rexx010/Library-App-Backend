f4fc49c4ad993007a61301e1c4bb36878e625985
rexx010 <grexx200@gmail.com>
2025-09-15 12:38:23 +0100

    updated UserService: added login and findById methods to enhance user management functionality

--- src/main/java/com/rexxempire/services/UserService.java
+++ src/main/java/com/rexxempire/services/UserService.java
@@ -1,10 +1,14 @@
 package com.rexxempire.services;
 
 import com.rexxempire.data.models.User;
+import com.rexxempire.dtos.requests.LoginRequest;
 import com.rexxempire.dtos.requests.UserRequest;
 import com.rexxempire.dtos.responses.UserResponse;
 
+import java.util.Optional;
+
 public interface UserService {
     UserResponse registerUser(UserRequest userRequest);
-    User validateUser(String email, String password);
+    Optional<User> login(LoginRequest loginRequest);
+    Optional<User> findById(String id);
 }
