8b04895280aabf58e24fe4941aa220e385c24799
rexx010 <grexx200@gmail.com>
2025-09-15 08:37:27 +0100

    updated BorrowBookRepository: changed return type of findByUserId to List for handling multiple borrow records

--- src/main/java/com/rexxempire/data/repositories/BorrowBookRepository.java
+++ src/main/java/com/rexxempire/data/repositories/BorrowBookRepository.java
@@ -4,10 +4,11 @@ import com.rexxempire.data.models.BorrowBook;
 import com.rexxempire.data.models.Status;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BorrowBookRepository extends MongoRepository <BorrowBook, String>{
-    Optional<BorrowBook> findByUserId(String userId);
+    List<BorrowBook> findByUserId(String userId);
     Optional<BorrowBook> findByUserIdAndBookIdAndStatus(String userId, String bookId, Status status);
     Optional<BorrowBook> findFirstByUserIdAndBookIdAndStatus(String userId, String bookId, Status status);
 }
