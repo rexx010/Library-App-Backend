9beb8a660dca17ea4508416ebc2da28365d05818
rexx010 <grexx200@gmail.com>
2025-09-15 08:27:14 +0100

    added @Valid annotation to AuthController methods for request validation and simplified error handling

--- src/main/java/com/rexxempire/controllers/AuthController.java
+++ src/main/java/com/rexxempire/controllers/AuthController.java
@@ -5,6 +5,7 @@ import com.rexxempire.dtos.requests.LoginRequest;
 import com.rexxempire.dtos.requests.UserRequest;
 import com.rexxempire.services.UserServiceImp;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +18,12 @@ public class AuthController {
     private UserServiceImp userService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody UserRequest userRequest) {
-        try {
-            return ResponseEntity.ok(userService.registerUser(userRequest));
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(400).body(e.getMessage());
-        }
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserRequest userRequest) {
+        return ResponseEntity.ok(userService.registerUser(userRequest));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpSession session) {
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest, HttpSession session) {
         return userService.login(loginRequest)
                 .map(user -> {
                     session.setAttribute("userId", user.getId());
