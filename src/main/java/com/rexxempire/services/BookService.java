37d41a035526ed6a67c1ca1d9b9898b3480c35d8
rexx010 <grexx200@gmail.com>
2025-09-15 12:37:14 +0100

    refactored BookService: converted class implementation to an interface to abstract service layer logic

--- src/main/java/com/rexxempire/services/BookService.java
+++ src/main/java/com/rexxempire/services/BookService.java
@@ -1,58 +1,15 @@
 package com.rexxempire.services;
 
-import com.rexxempire.data.models.Author;
 import com.rexxempire.data.models.Book;
-import com.rexxempire.data.repositories.AuthorRepository;
-import com.rexxempire.data.repositories.BookRepository;
 import com.rexxempire.dtos.requests.BookRequest;
 import com.rexxempire.dtos.responses.BookResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class BookService {
-    @Autowired
-    private BookRepository bookRepository;
-    @Autowired
-    private AuthorRepository authorRepository;
-
-    public BookResponse addBook(String authorId, BookRequest bookRequest){
-        Author author = authorRepository.findById(authorId)
-                .orElseThrow(() -> new RuntimeException("Author not found with id : " + authorId));
-        Book book = new Book();
-        book.setIsbn(bookRequest.getIsbn());
-        book.setTitle(bookRequest.getTitle());
-        book.setAuthor(author);
-        book.setGenre(bookRequest.getGenre());
-        book.setPublishedDate(bookRequest.getPublishedDate());
-        book.setTotalCopies(bookRequest.getTotalCopies());
-        book.setAvailableCopies(bookRequest.getAvailableCopies());
-        bookRepository.save(book);
-
-        BookResponse bookResponse = new BookResponse();
-        bookResponse.setId(book.getId());
-        bookResponse.setTitle(book.getTitle());
-        bookResponse.setIsbn(book.getIsbn());
-        bookResponse.setPublishedDate(book.getPublishedDate().toString());
-        bookResponse.setAuthorName(book.getAuthor());
-        bookResponse.setGenre(book.getGenre());
-        bookResponse.setTotalCopies(book.getTotalCopies());
-        bookResponse.setAvailableCopies(book.getAvailableCopies());
-
-        return bookResponse;
-    }
-
-    public List<Book> getAllBooks(){
-        return bookRepository.findAll();
-    }
-
-    public Optional<Book> getBookById(String id){
-        return bookRepository.findById(id);
-    }
-    public void deleteBookById(String id){
-        bookRepository.deleteById(id);
-    }
+public interface BookService {
+    BookResponse addBook(String authorId, BookRequest bookRequest);
+    List<Book> getAllBooks();
+    Optional<Book> getBookById(String id);
+    void deleteBookById(String id);
 }
