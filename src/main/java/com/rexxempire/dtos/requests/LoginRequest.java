5dbaa2e37c2d5056c2f66c5c5d69177c7a32f6f4
rexx010 <grexx200@gmail.com>
2025-09-15 08:38:40 +0100

    added validation annotations to LoginRequest fields to enforce non-null constraints

--- src/main/java/com/rexxempire/dtos/requests/LoginRequest.java
+++ src/main/java/com/rexxempire/dtos/requests/LoginRequest.java
@@ -1,9 +1,12 @@
 package com.rexxempire.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @NotBlank(message = "Username must not be null or empty")
     private  String username;
+    @NotBlank(message = "Password must not be null or empty")
     private  String password;
 }
