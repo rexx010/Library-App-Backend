160fefcc8ac9b5a338bd383631b2cf9019d86845
rexx010 <grexx200@gmail.com>
2025-09-15 08:35:26 +0100

    added endpoints in BookReservationController for creating and canceling book reservations

--- src/main/java/com/rexxempire/controllers/BookReservationController.java
+++ src/main/java/com/rexxempire/controllers/BookReservationController.java
@@ -1,10 +1,36 @@
 package com.rexxempire.controllers;
 
+import com.rexxempire.data.models.BookReservation;
+import com.rexxempire.services.BookReservationService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/book-reservations")
 public class BookReservationController {
+    @Autowired
+    private BookReservationService bookReservationService;
 
+    @PostMapping("/{bookId}")
+    public ResponseEntity<?> reservation(@PathVariable("bookId") String bookId, HttpSession session){
+        String userId = (String) session.getAttribute("userId");
+        if(userId == null){
+            return ResponseEntity.status(401).body("No user found");
+        }
+        return ResponseEntity.ok(bookReservationService.reservation(userId, bookId));
+    }
+
+    @PostMapping("/cancel/{bookId}")
+    public ResponseEntity<?> cancelReservation(@PathVariable("bookId") String bookId, HttpSession session){
+        String userId = (String) session.getAttribute("userId");
+        if(userId == null){
+            return ResponseEntity.status(401).body("No user found");
+        }
+        return ResponseEntity.ok(bookReservationService.deleteReservation(userId, bookId));
+    }
 }
