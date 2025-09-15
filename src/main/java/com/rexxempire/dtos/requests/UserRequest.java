d491342aabe743807aeb7164b149d24ffc70b596
rexx010 <grexx200@gmail.com>
2025-09-15 08:39:24 +0100

    added spring-boot-starter-validation dependency to enable request validation

--- src/main/java/com/rexxempire/dtos/requests/UserRequest.java
+++ src/main/java/com/rexxempire/dtos/requests/UserRequest.java
@@ -1,11 +1,18 @@
 package com.rexxempire.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UserRequest {
+    @NotBlank(message = "Username must not be null or empty")
     private String username;
+    @NotBlank(message = "Email must not be null or empty")
+    @Pattern(regexp = "^[A-Za-z0-9]+@[A-Za-z]+\\.com$", message = "Invalid email address")
     private String email;
+    @NotBlank(message = "Password must not be null or empty")
     private String password;
+    @NotBlank(message = "Role must not be null or empty")
     private String role;
 }
