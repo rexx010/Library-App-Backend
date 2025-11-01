ec20bd1fc5e55d59d70ea924fdc889349ff0ac8a
rexx010 <grexx200@gmail.com>
2025-11-01 23:19:46 +0100

    added getAllBookReservations endpoint to retrieve user-specific book reservations

--- src/main/java/com/rexxempire/controllers/BookReservationController.java
+++ src/main/java/com/rexxempire/controllers/BookReservationController.java
@@ -4,16 +4,22 @@ import com.rexxempire.services.BookReservationServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/book-reservations")
 public class BookReservationController {
     @Autowired
     private BookReservationServiceImpl bookReservationService;
+
+    @GetMapping("/reserved")
+    private ResponseEntity<?> getAllBookReservations(HttpSession session){
+        String userId = (String) session.getAttribute("userId");
+        if(userId == null){
+            throw new RuntimeException("No user logged in");
+        }
+        return ResponseEntity.ok(bookReservationService.getReservationsByUser(userId));
+    }
 
     @PostMapping("/{bookId}")
     public ResponseEntity<?> reservation(@PathVariable("bookId") String bookId, HttpSession session){
